bf98fecc8bdb7072a38ee588502ed860b873ff2c
Tiago da Silva Moura <tiago.silva991@outlook.com>
2021-10-11 16:28:46 -0300

    adicionando exercicios006.py

---
+++ exercicio006.py
@@ -0,0 +1,8 @@
+m = float(input('Uma distancia em metros: '))
+km = m/1000
+hm = m/100
+dc = m/10
+dm = m*10
+cm = m*100
+ml = m*1000
+print('{}km \n{}hm \n{}dc \n{}dm \n{}cm \n{}mm'.format(km, hm, dc, dm, cm, ml))
