f9ea19ea4d65740ea2def83284cfd9a26cd8df0b
Tiago da Silva Moura <tiago.silva991@outlook.com>
2021-10-19 14:07:53 -0300

    adicionando seno_cosseno_tangente.py

---
+++ seno_cosseno_tangente.py
@@ -0,0 +1,8 @@
+from math import radians, cos, sin, tan
+angulo = float(input('Digite o Ângulo que você deseja: '))
+seno = sin(radians(angulo))
+print(f'O ângulo de {angulo} tem o SENO de {seno:.2f}')
+cosseno = cos(radians(angulo))
+print(f'O ângulo de {angulo} tem o COSSENO de {cosseno:.2f}')
+tangente = tan(radians(angulo))
+print(f'O ângulo de {angulo} tem a TANGENTE de {tangente:.2f}')
