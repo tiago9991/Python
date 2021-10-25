e764a229f203db1ffa06b24db3871b59f384d59d
Tiago da Silva Moura <tiago.silva991@outlook.com>
2021-10-25 12:53:10 -0300

    adicionando sorteio_de_ordem_de_lista.py

---
+++ sorteio_de_ordem_de_lista.py
@@ -0,0 +1,10 @@
+from random import shuffle
+print('Vamos sortear para ver a ordem')
+aluno1 = str(input('Primeiro aluno: '))
+aluno2 = str(input('Segundo aluno: '))
+aluno3 = str(input('Terceiro aluno: '))
+aluno4 = str(input('Quarto aluno: '))
+lista = [aluno1, aluno2, aluno3, aluno4]
+shuffle(lista)
+print('A ordem de apresentação será')
+print(lista)
