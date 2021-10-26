8008e324dc6d9558ca339dee05b9ebdc47744bc1
Tiago da Silva Moura <tiago.silva991@outlook.com>
2021-10-26 15:07:20 -0300

    adicionando analisando_texto.py

---
+++ analisando_texto.py
@@ -0,0 +1,7 @@
+nome = str(input('Digite seu nome completo: ')).strip()
+print('Analisando seu nome...')
+print(f'Seu nome em maiúsculas é {nome.upper()} ')
+print(f'Seu nome em minúsculas é {nome.lower()}')
+print(f'Seu nome tem ao todo {len(nome) - nome.count(" ")} letras')
+separa = nome.split()
+print(f'Seu primeiro nome é {separa[0]} e ele tem {len(separa[0])} letras')
